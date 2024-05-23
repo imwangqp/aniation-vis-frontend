a1bc5880bb7ecee0eb9734cf346e17fe76ee9aea
wangqip <wangqipengscu@outlook.com>
2024-05-23 10:00:27 +0800

    5.23用新数据绘制的

--- src/components/MapProjection.vue
+++ src/components/MapProjection.vue
@@ -1,26 +1,20 @@
 <script setup>
-import {onMounted, ref} from "vue";
+import {onMounted} from "vue";
 import * as d3 from "d3";
-import shp from 'shpjs'
 import _ from 'lodash'
 
-const geoJsonPath = '../assets/routes/output.geojson'
+const geoJsonPath = '../assets/routes/output1.geojson'
 
 onMounted(()=>{
   const geoJSONUrl = new URL(geoJsonPath, import.meta.url).href;
   fetch(geoJSONUrl)
       .then(response => response.json())
       .then(data => {
-        selectOptions = _(data.features)
-            .map(feature => _.get(feature, 'properties.seg_id'))
-            .uniq()
-            .value();
-        console.log(selectOptions)
-        drawMap(data, 0)
+        drawMap(data)
       });
 })
 
-function drawMap(geojsonData, segment_id) {
+function drawMap(geojsonData) {
   let max_count = _.maxBy(geojsonData.features,o=>o['properties']['count'])['properties']['count'],
       min_count = _.minBy(geojsonData.features,o=>o['properties']['count'])['properties']['count']
   const svg = d3.select("#map").append("svg")
@@ -28,31 +22,58 @@ function drawMap(geojsonData, segment_id) {
       .attr("height", 600);
   const projection = d3.geoIdentity().reflectY(true);
   const path = d3.geoPath().projection(projection);
-  const colorScale = d3.scaleSequential(d3.interpolateSpectral)
-      .domain([min_count, max_count]);
+  let widthScale = d3.scaleLinear([min_count,max_count], [1,5])
   projection.fitSize([800, 600], geojsonData);
-  // _.filter(geojsonData.features, { 'properties':{'seg_id':segment_id} })
+
+  geojsonData.features.forEach(function(feature, i) {
+    let linearGradient = svg.append("defs")
+        .append("linearGradient")
+        .attr("id", "line-gradient-" + i) // 为每个渐变创建唯一的ID
+        .attr("gradientUnits", "userSpaceOnUse"); // 使用用户坐标系统
+
+// 添加起始色（红色）
+    linearGradient.append("stop")
+        .attr("offset", "0%")
+        .attr("stop-color", "#C6FA7A");
+
+// 添加结束色（蓝色）
+    linearGradient.append("stop")
+        .attr("offset", "100%")
+        .attr("stop-color", "#7A7863");
+
+// 假设你的feature.geometry.coordinates是一个数组，其中第一个元素是起点，最后一个元素是终点
+// 你需要将这些坐标转换为屏幕上的点
+    let startPoint = feature.geometry.coordinates[0];
+    let endPoint = feature.geometry.coordinates[feature.geometry.coordinates.length - 1];
+
+// 转换坐标到屏幕空间（这里假设你有一个projection函数来进行转换）
+    let projectedStart = projection(startPoint);
+    let projectedEnd = projection(endPoint);
+
+// 设置渐变的起点和终点
+    linearGradient.attr("x1", projectedStart[0])
+        .attr("y1", projectedStart[1])
+        .attr("x2", projectedEnd[0])
+        .attr("y2", projectedEnd[1]);
+  });
+
   svg.selectAll('.line')
       .data(geojsonData.features)
       .enter()
       .append('path')
       .attr('class', 'line')
       .attr('d', path)
-      .attr("fill", d=>colorScale(d.properties.count))
-      .attr('stroke', 'black')
+      .attr("fill", "url(#line-gradient)")
+      .attr('stroke', function(d, i) {
+        return "url(#line-gradient-" + i + ")"; // 引用对应的渐变ID
+      })
+      .attr('stroke-width', d=>widthScale(d.properties.count))
 }
 
 </script>
 
 <template>
-<!--  <el-select v-model="value" placeholder="Select" style="width: 240px">-->
-<!--    <el-option-->
-<!--        v-for="item in options"-->
-<!--        :key="item.value"-->
-<!--        :label="item.label"-->
-<!--        :value="item.value"-->
-<!--    />-->
-<!--  </el-select>-->
+
   <div id="map"></div>
 </template>
 
@@ -60,10 +81,5 @@ function drawMap(geojsonData, segment_id) {
 #map {
   width: 800px;
   height: 600px;
-}
-.line {
-  fill: none;
-  stroke: black;
-  stroke-width: 2px;
 }
 </style>
